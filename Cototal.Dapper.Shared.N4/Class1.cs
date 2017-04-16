33e73710efa64440046bf2e4d86b23398ac3b50b
Shaun T Martin <webmaster@ginblades.com>
2017-04-16 16:00:07 -0500

    Add project files.

---
+++ Cototal.Dapper.Shared.N4/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cototal.Dapper.Shared.N4
+{
+    public class Class1
+    {
+    }
+}
